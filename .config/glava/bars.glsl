457ef22f1d75b94eac527afcd6b0cbadc6b2bb6b
trefmanic <trefmanic@gmail.com>
2019-10-25 17:20:27 +0500

    Solid colors for glava? TODO: check how GLSL works

--- .config/glava/bars.glsl
+++ .config/glava/bars.glsl
@@ -17,7 +17,7 @@
 /* How quickly the gradient transitions, in pixels */
 #define GRADIENT 80
 /* Bar color. By default this provides a blue-white gradient. */
-#define COLOR @fg:mix(#7f9f7f,#06989a, clamp(d / GRADIENT, 0, 1))
+#define COLOR @fg:mix(#7F9F7F,#7F9F7F, clamp(d / GRADIENT, 0, 1))
 /* Outline color. By default this provides a 'glint' outline based on the bar color */
 #define BAR_OUTLINE @bg:vec4(COLOR.rgb * 1.5, COLOR.a)
 /* Direction that the bars are facing, 0 for inward, 1 for outward */
